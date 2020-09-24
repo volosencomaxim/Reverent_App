90610b93ebb87c9bd3da8f48a5ef5d70d9fcf07d
Maxim Volosenco <maxim.volosenco@extendaretail.com>
2020-09-24 11:32:18 +0300

    nimor modifications

--- Controllers/BurlacuController.cs
+++ Controllers/BurlacuController.cs
@@ -15,7 +15,7 @@ namespace Reverent_App.Controllers
 {
     class BurlacuController
     {
-        string staringURL;
+        string rootURL;
         string accessToken;
 
 
@@ -25,10 +25,10 @@ namespace Reverent_App.Controllers
 
         public BurlacuController()
         {
-            staringURL = Global.AppSetUp.RootURL + "/register";
+            rootURL = Global.AppSetUp.RootURL + "/register";
             extractService = new ExtractService();
             
-            dataConrainer = extractService.GetRequest(staringURL);
+            dataConrainer = extractService.GetRequest(rootURL);
             accessToken = dataConrainer["access_token"].ToString();
         }
         public JObject HomeRoute()
@@ -39,9 +39,6 @@ namespace Reverent_App.Controllers
             var firstJson = extractService.GetRequest(nextLink, accessToken);
 
             return firstJson;
-            //Console.WriteLine(token);
-            //Console.WriteLine(nextLink);
-
         }
 
 
@@ -61,45 +58,15 @@ namespace Reverent_App.Controllers
         public void GetRequest(JObject entireJson)
         {
 
-            //string secondURL = Global.AppSetUp.RootURL + "/route/1";
-            //string newURL = Global.AppSetUp.RootURL + dataConrainer["link"].ToString();
-
-
-            //string accessToken = dataConrainer["access_token"].ToString();
-
-            //var entireJson = extractService.GetRequest(secondURL, accessToken);
-
-            //var entireJson = HomeRoute();
-
-            //Console.WriteLine(entireJson);
-
-            //if (entireJson["data"] != null)
-            //{
-            //    Console.WriteLine(entireJson["data"]);
-            //}
-
-            //if (entireJson["mime_type"] != null)
-            //{
-            //    Console.WriteLine(entireJson["mime_type"]);
-            //}
-            //if (entireJson["data"] != null && entireJson["mime_type"] == null)
-            //{
-            //    Console.WriteLine("Data is of string type!!");
-            //}
-
-            //Console.WriteLine(entireJson.ToString());
-
             if (entireJson["link"] != null)
             {
                 var linkJson = JObject.Parse(entireJson["link"].ToString());
 
                 foreach (JProperty property in linkJson.Properties())
                 {
-                    //Console.WriteLine(property.Value);
                     string nextLink = Global.AppSetUp.RootURL + property.Value;
 
-                    Console.WriteLine(nextLink);
-
+                    //Console.WriteLine(nextLink);
 
                     ThreadPool.QueueUserWorkItem(state => Checker(nextLink, accessToken));
                 }
@@ -116,35 +83,23 @@ namespace Reverent_App.Controllers
             var entireJson = extractService.GetRequest(nextLink, accessToken);
             //Console.WriteLine(entireJson);
 
-            //if (entireJson["data"] != null)
-            //{
-            //    Console.WriteLine(entireJson["data"]);
-            //}
+            if (entireJson["mime_type"] != null && entireJson["data"] != null)
+            {
+                Console.WriteLine(entireJson["mime_type"].ToString() + " : {\n" +  entireJson["data"].ToString() + "\n}");
+            }
 
             //if (entireJson["mime_type"] != null)
             //{
             //    Console.WriteLine(entireJson["mime_type"]);
             //}
-            //if (entireJson["data"] != null && entireJson["mime_type"] == null)
-            //{
-            //    Console.WriteLine("Data is of string type!!");
-            //}
+            if (entireJson["mime_type"] != null && entireJson["data"] == null)
+            {
+                Console.WriteLine("text/String : {\n" + entireJson["data"].ToString() + "\n}");
+            }
 
             if (entireJson["link"] != null)
-            {
-                //var linkJson = JObject.Parse(entireJson["link"].ToString());
-
-                //Console.WriteLine("LINKKKKKKKKK ==============  " + linkJson.ToString());
-
                 GetRequest(entireJson);
-                //foreach (JProperty property in linkJson.Properties())
-                //{
-                //    //Console.WriteLine(property.Value);
-                //    string nextOne = Global.AppSetUp.RootURL + property.Value;
-
-                //    GetRequest();
-                //}
-            }
+            
         }
 
     }
